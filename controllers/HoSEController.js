b4dc2eb8ba4578740043e515316d56bfe1bd0edf
hitd01 <tranhuan.dev@gmail.com>
2023-03-19 22:33:17 +0700

    add check HoSE ticker and not HoSE ticker

--- controllers/HoSEController.js
+++ controllers/HoSEController.js
@@ -26,18 +26,34 @@ export const getTickerHistoryData = async (req, res) => {
     await page.type('#ContentPlaceHolder1_ctl03_dpkTradeDate2_txtDatePicker', end_date);
     await page.waitForSelector('#ContentPlaceHolder1_ctl03_btSearch');
     await page.click('#ContentPlaceHolder1_ctl03_btSearch');
-    // await page.waitForNavigation({ timeout: 60 * 1000 * 1 });
+
+    let tableId = 'GirdTable';
+    const table = await page.$(`#${tableId}`);
+    if (!table) {
+      tableId = 'GirdTable2';
+    }
 
     let ticker_history_data = [];
     let flag = true;
     while (flag) {
-      const tickerData = await page.$$eval('#GirdTable2 > tbody > tr', (trs) => {
-        return trs.slice(2).map((tr) => {
-          const tds = Array.from(tr.querySelectorAll('td'));
-          const tdContents = tds.map((td) => td.textContent.trim());
-          return [...tdContents.slice(0, 4), ...tdContents.slice(5, 12)];
+      let tickerData = [];
+      if (tableId === 'GirdTable') {
+        tickerData = await page.$$eval(`#${tableId} > tbody > tr`, (trs) => {
+          return trs.slice(2).map((tr) => {
+            const tds = Array.from(tr.querySelectorAll('td'));
+            const tdContents = tds.map((td) => td.textContent.trim());
+            return [...tdContents.slice(0, 5), ...tdContents.slice(6)];
+          });
         });
-      });
+      } else {
+        tickerData = await page.$$eval(`#${tableId} > tbody > tr`, (trs) => {
+          return trs.slice(2).map((tr) => {
+            const tds = Array.from(tr.querySelectorAll('td'));
+            const tdContents = tds.map((td) => td.textContent.trim());
+            return [...tdContents.slice(0, 4), ...tdContents.slice(5, 12)];
+          });
+        });
+      }
       ticker_history_data = [...ticker_history_data, ...tickerData];
       console.log(ticker_history_data);
       // await new Promise((resolve) => setTimeout(resolve, 600));
@@ -49,7 +65,7 @@ export const getTickerHistoryData = async (req, res) => {
       if (nextPageIcon === '>') {
         await page.waitForSelector('table.CafeF_Paging > tbody > tr > td:last-child > a');
         await page.click('table.CafeF_Paging > tbody > tr > td:last-child > a');
-        await page.waitForSelector('#GirdTable2 > tbody > tr');
+        await page.waitForSelector(`#${tableId} > tbody > tr`);
       } else {
         flag = false;
       }
